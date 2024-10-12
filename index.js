62f9d611e96c18cb3aa4d77648167f5dd9107705
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-10-12 10:44:32 +0500

    setted env file, api get and post routes!

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require ('express');
 const app = express();
-app.listen(8080, function(){
-    console.log("server started running on port 8080!")
+app.set('view engine', 'ejs');
+require('dotenv').config();
+app.get('/', function(request,result){
+    console.log("hello");
+   result.render('Home');
+
+})
+app.get('/about', function(request, result){
+    result.status(200).json({message:"Aboutpage"})
+})
+app.post('/user/store', function(req, res){
+    res.status(200).json({message:"Data submitted"})
+})
+app.listen(process.env.PORT, function(){
+    console.log(`server started running on port ${process.env.PORT}!`)
 })
