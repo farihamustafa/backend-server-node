87dd6dcfedc3102f3d694765d832decd31b04b45
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-11-11 10:29:37 -0800

    added request

---
+++ requests/userReq.js
@@ -0,0 +1,40 @@
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User'); // Adjust the path as necessary
+
+class userReq {
+  static validationRules(update=false) {
+    const rules = [
+      body('name').isString().withMessage('Name must be a string'),
+      body('email')
+        .isEmail().withMessage('Email must be a valid email address')
+        .custom(async (value, { req }) => {
+          const user = await User.findOne({ email: value });
+          if (user) {
+            if(update && req.params.id === user._id.toString()){
+              return;
+            }
+            return Promise.reject('Email already in use');
+          }
+        }),  
+    ];
+    if (!update) {
+        rules.push(
+            body('password')
+              .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/, "i")
+              .withMessage('Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character.')
+          );
+             }
+    return rules;
+
+  }
+
+  static validate(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+}
+
+module.exports = userReq;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const Authentication = require('../middlewares/Authentication');
+const userReq = require('../requests/userReq');
+
 const authRouter = express.Router();
-authRouter.post('/login',Authentication.login);
-authRouter.post('/register',Authentication.register);
-module.exports = authRouter;
+
+authRouter.post('/login',Authentication.login); //create
+authRouter.post('/register',userReq.validationRules(),userReq.validate,Authentication.register);
+
+module.exports=authRouter;
+
