6d14e96ceacf00fef99183b53831daf9e39b30be
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-11-27 10:23:07 -0800

    updated

--- middlewares/Authentication.js
+++ middlewares/Authentication.js
@@ -1,64 +1,88 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const userServices = require('../services/userServices');
+const userService = require('../services/userService');
+require("dotenv").config();
 
-require ('dotenv').config();
+
 const key = process.env.KEY;
-let blacklist=[];
-class Authentication{
-    VerifyToken(req,res,next){
-        const authHeader = req.headers['authorization'];
-        if(!authHeader)return res.status(403).json({message:'No token provided'});
-        const token = authHeader.split(' ')[1];
-        if(!token)return res.status(403).json({message:'No token provided'});
 
-        if(blacklist.includes(token)){
-            return res.status(401).json({message:'Token revoked. Please login again'});
+let blacklist = [];
+
+class Authentication{
+
+    verifyToken(req,res,next){
+
+
+        const authHeader = req.headers['authorization'];
+        if (!authHeader) return res.status(403).json({ message: 'No token provided.' });
+
+        const token = authHeader.split(' ')[1];
+        if (!token) return res.status(403).json({ message: 'No token provided.' });
+
+        if (blacklist.includes(token)) {
+            return res.status(401).json({ message: 'Token revoked. Please login again.' });
         }
-        jwt.verify(token, key,(err,decoded)=>{
-            if(err){
-                return res.status(401).json({message:"Failed to authenticate token"})
+
+        jwt.verify(token,key, (err,decoded)=>{
+            if (err) {
+                return res.status(401).json({ message: 'Failed to authenticate token.' });
             }
-            req.user= decoded;
+            req.user = decoded;
             next();
-        })
+        } )
+
     }
 
+    verify(req,res){
+        const authHeader = req.headers['authorization'];
+        if (!authHeader) return res.status(403).json({ message: 'No token provided.' });
 
-    ////LOGON IN FUNCTION
+        const token = authHeader.split(' ')[1];
+        if (!token) return res.status(403).json({ message: 'No token provided.' });
+
+        if (blacklist.includes(token)) {
+            return res.status(401).json({ message: 'Token revoked. Please login again.' });
+        }
+
+        jwt.verify(token,key, (err,decoded)=>{
+            if (err) {
+                return res.status(401).json({ message: 'Failed to authenticate token.' });
+            }
+            req.user = decoded;
+            return res.status(200).json({message:"Valid token",user:req.user})
+        } )
+  
+    }
+
     async login(req,res){
-        try {
-            const credentials =(({email , password})=>({email, password}))(req.body);
+        try{
+            const credentials = (({email,password})=>({email,password}))(req.body);
             const user = await User.findOne(credentials);
             if(!user){
-                res.status(400).json({message: "User not found"})
+                return  res.status(400).json({message:"User not found!" })
             }
-            const token = jwt.sign({email:user.email, name :user.name , id :user.id}, key, {expiresIn :'1h'});
-            res.status(200).json({token});
-
-            
-        } 
-        catch (err) {
-            res.status(400).json({message:err.message})
-            
+            const token = jwt.sign({  email : user.email , name : user.name , id : user.id  }, key, { expiresIn: '1h' });
+           return res.status(200).json({token});
         }
+        catch(err){
+            return  res.status(400).json({message:err.message })
+        }      
     }
 
-    ///REGISTER
     async register(req,res){
-    userServices.create(req, res);
+        userService.create(req,res);
     }
 
-    
-    logout(req, res){
+    logout(req,res){  
         const authHeader = req.headers['authorization'];
-        if(!authHeader) return res.status(403).json({message: 'No token provided'})
+        if (!authHeader) return res.status(403).json({ message: 'No token provided.' });
 
         const token = authHeader.split(' ')[1];
-        if(!token) return res.status(403).json({message:'No token provided'});
+        if (!token) return res.status(403).json({ message: 'No token provided.' });
+        
         blacklist.push(token);
-        res.status(200).json({message:'Logged out successfully'});
+        return res.status(200).json({ message: 'Logged out successfully.' });
     }
-
 }
-module.exports= new Authentication
+
+module.exports = new Authentication;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const Authentication = require('../middlewares/Authentication');
 const userReq = require('../requests/userReq');
@@ -7,5 +8,6 @@ const authRouter = express.Router();
 authRouter.post('/login',Authentication.login); //create
 authRouter.post('/register',userReq.validationRules(),userReq.validate,Authentication.register);
 authRouter.post('/logout',Authentication.logout);
+authRouter.post('/verify',Authentication.verify);
 module.exports=authRouter;
 
